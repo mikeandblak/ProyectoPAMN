ed61a99b0558416adcc6b6d9e42531e6ae59a66e
Luis Vilchis <luis.oropeza.129@gmail.com>
2015-05-25 19:55:49 -0500

    Ejemplo de Cronología generada.

--- Peticiones/Peticiones.py
+++ Peticiones/Peticiones.py
@@ -10,12 +10,19 @@ class Peticiones:
         pass
 
     @classmethod
-    def pedir_numero(cls, mensaje, rango):
+    def pedir_numero(cls, mensaje, limite_izquierda, limite_derecha):
+        """
+        Función que pide número y lo devuelve
+        :param mensaje: String que se mostrará para pedir número
+        :param limite_izquierda: Número más pequeño que será aceptado
+        :param limite_derecha: Número más grande que será aceptado
+        :return: Número leído
+        """
         while True:
             numero = raw_input(mensaje)
             try:
                 numero = int(numero)
-                if rango[0] <= numero <= rango[1]:
+                if limite_izquierda <= numero <= limite_derecha:
                     return numero
                 else:
                     print('Número no válido.')
@@ -26,6 +33,11 @@ class Peticiones:
 
     @classmethod
     def es_numero(cls, numero):
+        """
+        Función que verifica sí el parametro recibido es un número o no
+        :param numero:
+        :return: Si es número True
+        """
         try:
             num = int(numero)
             return num
@@ -35,6 +47,9 @@ class Peticiones:
 
     @classmethod
     def limpiar_pantalla(cls):
+        """
+        Función que limpia la pantalla del shell(Unix) o consola(Windows)
+        """
         if os.name == "posix":
             os.system("clear")
         elif os.name == ("ce", "nt", "dos"):
@@ -42,6 +57,10 @@ class Peticiones:
 
     @classmethod
     def copiar_archivos_responsive(cls, nombre_proyecto):
+        """
+        Copia los archivos necesarios para que la página HTML se vea correctamente
+        :param nombre_proyecto: Nombre de la carpeta del proyecto generado
+        """
         try:
             a = sys.path
             archivos = str(a[0])+'/Responsive'
@@ -52,6 +71,11 @@ class Peticiones:
 
     @classmethod
     def log_twitter(cls, error):
+        """
+        Log de errores, relacionado a Twitter
+        :param error: Error que se guardará
+        """
+
         try:
             archivo_log = open('Logs/twitter.error', 'a+')
             archivo_log.write(error + '\n')
@@ -61,6 +85,10 @@ class Peticiones:
 
     @classmethod
     def log_instagram(cls, error):
+        """
+        Log de errores, relacionado a Instagram
+        :param error: Error que se guardará
+        """
         try:
             archivo_log = open('Logs/instagram.error', 'a+')
             archivo_log.write(error + '\n')
@@ -70,6 +98,10 @@ class Peticiones:
 
     @classmethod
     def log(cls, error):
+        """
+        Log de errores generales
+        :param error: Error que se guardará
+        """
         try:
             archivo_log = open('Logs/log.error', 'a+')
             archivo_log.write(error + '\n')
